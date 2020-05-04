277e443bccf80a682b3d750c9ef3a1b050d86057
Achilles <achipap@hotmail.com>
2020-05-04 21:50:44 +0200

    Update helix.js

--- helix.js
+++ helix.js
@@ -36,7 +36,7 @@ function draw() {
   pop();
   console.log(vol);
   
-  if(mouseIsPressed || vol > 0.15){
+  if(mouseIsPressed || vol > 0.10){
     h.fly(15);
     if(charge < 70){
       charge += 1;
